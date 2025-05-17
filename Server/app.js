58390dfa48338a7f2baae3f6caec1dccd4038e94
praveen choudhary <pc852180@gmail.com>
2025-05-17 15:06:49 +0530

    Shose-E-Commerce-Site

---
+++ Server/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+app.use(cors());
+// Parse incoming requests with JSON payloads
+app.use(bodyParser.json());
+
+// Parse incoming requests with urlencoded payloads
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect(process.env.CONNECTION_STRING).then(()=>{
+    console.log("DB IS CONNECTED")
+})
+
+
+
+
+
+
+
+const port = process.env.PORT;
+
+app.listen(port, ()=>{
+    console.log(`Server is Running on ${port} port`);
+})
